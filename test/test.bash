916be2f9ffdc988fd97971f2bce566c18fa4b6b7
Fujitake Hiroto <hiroto180205@icloud.com>
2026-01-11 18:46:37 +0900

    正常な入力の処理を追加

--- test/test.bash
+++ test/test.bash
@@ -6,9 +6,51 @@ dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
-colcon build
+colcon build --packages-select virtual_travel
 source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
+CSV_PATH="install/virtual_travel/share/virtual_travel/config/location.csv"
+res=0
+
+ng () {
+    echo "Error at line $1"
+    res=1
+}
+
+cp $CSV_PATH /tmp/location.csv.bak
+
+# 正常な入力
+echo "name,latitude,longitude" > $CSV_PATH
+echo "Tokyo,35.0,139.0" >> $CSV_PATH
+echo "Osaka,34.0,135.0" >> $CSV_PATH
+
+ros2 run virtual_travel gnss_simulator > /tmp/gnss.log 2>&1 &
+NODE_PID=$!
+sleep 3
+
+out=$(ros2 topic echo /nearest_location --once --field data)
+
+if [ $? -ne 0 ]; then
+    echo "Command failed: ros2 topic echo"
+    ng "$LINENO"
+fi
+
+echo "Test1 Output: $out"
+
+if [ "$out" != "Osaka" ]; then
+    ng "$LINENO"
+fi
+
+kill $NODE_PID
+
+cp /tmp/location.csv.bak $CSV_PATH
+
+#CSVファイルがない場合の入力
+
+if [ $res -eq 0 ]; then
+    echo "ok"
+    exit 0
+else
+    echo "failed"
+    exit 1
+fi
